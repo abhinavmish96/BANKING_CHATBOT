1454b8ab13041d433957f52715c688cbe8d6781d
Abhinav Mishra <abhinavmish96@gmail.com>
2019-09-11 08:26:57 +0530

    server

---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const server = require('./app');
+const port = process.env.PORT || process.env.VCAP_APP_PORT || 3000;
+
+server.listen(port, function() {
+  console.log('Server running on port: %d', port);
+});
